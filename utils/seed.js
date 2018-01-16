db8ccfe96aaefb699fc15c96d2c26d826f6385bf
Benson Maruchu <benmaruchu@gmail.com>
2018-01-16 12:34:48 +0300

    add  ability to seed default statuses

--- utils/seed.js
+++ utils/seed.js
@@ -13,19 +13,73 @@
 //  dependencies
 const path = require('path');
 const _ = require('lodash');
+const async = require('async');
 const Status = require(path.join(__dirname, '..', 'models', 'status'));
+
+
+/**
+ * Run parallel saving statuses into the database
+ * @param {Array} statuses
+ * @param {Function} done
+ */
+function saveStatuses(statuses, done) {
+
+  statuses = _.map(statuses, function (status) {
+    return function (next) {
+      Status.findOneAndUpdate({
+        name: status.name
+      }, status, {
+        new: true,
+        upsert: true,
+        runValidators: true,
+        setDefaultsOnInsert: true
+      }, next);
+    };
+  });
+
+  // asynchronous save statuses
+  async.parallel(statuses, done);
+}
+
 
 module.exports = function (statuses, done) {
 
-  statuses = _.concat([], statuses);
+  const defaultStatuses = [{
+    name: 'Open',
+    color: '#2D93AD'
+  }, {
+    name: 'In Progress',
+    color: '#F6BD60'
+  }, {
+    name: 'Closed',
+    color: '#80B192'
+  }, {
+    name: 'Reopened',
+    color: '#FA8334'
+  }, {
+    name: 'Resolved',
+    color: '#14CC60'
+  }, {
+    name: 'Invalid',
+    color: '#3E4E50'
+  }, {
+    name: 'Duplicate',
+    color: '#33A1FD'
+  }];
 
-  Status.create(statuses, function (error, results) {
 
-    if (error) {
+  if (arguments.length === 1 && _.isFunction(arguments[0])) {
+    // seed defaults statuses
+    return saveStatuses(defaultStatuses, arguments[0]);
+  }
 
-      done(error);
-    }
+  statuses = _.compact(_.concat([], statuses));
 
-    done(null, results);
-  });
+  if (_.isEmpty(statuses)) {
+    // seed defaults seed defaults statuses
+    return saveStatuses(defaultStatuses, done);
+  }
+
+  // save provided statuses
+  return saveStatuses(statuses, done);
 };
