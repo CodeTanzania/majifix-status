e1b54554aacd85fba666f7668f09b74e93282dfc
lykmapipo <lallyelias87@gmail.com>
2019-08-19 17:34:54 +0300

    test: add getOneOrDefault specs

---
+++ test/integration/status.getOneOrDefault.spec.js
@@ -0,0 +1,57 @@
+import { expect, clear } from '@lykmapipo/mongoose-test-helpers';
+import { Status } from '../../src/index';
+
+describe.only('Status getOneOrDefault', () => {
+  before(done => clear(done));
+
+  let status = Status.fake();
+  status.default = true;
+
+  before(done => {
+    status.post((error, created) => {
+      status = created;
+      done(error, created);
+    });
+  });
+
+  it('should be able to get existing by id', done => {
+    const { _id } = status;
+    Status.getOneOrDefault({ _id }, (error, found) => {
+      expect(error).to.not.exist;
+      expect(found).to.exist;
+      expect(found._id).to.eql(status._id);
+      done(error, found);
+    });
+  });
+
+  it('should be able to get existing with criteria', done => {
+    const name = status.name.en;
+    Status.getOneOrDefault({ 'name.en': name }, (error, found) => {
+      expect(error).to.not.exist;
+      expect(found).to.exist;
+      expect(found._id).to.eql(status._id);
+      done(error, found);
+    });
+  });
+
+  it('should be able to get default with criteria', done => {
+    Status.getOneOrDefault({}, (error, found) => {
+      expect(error).to.not.exist;
+      expect(found).to.exist;
+      expect(found._id).to.eql(status._id);
+      done(error, found);
+    });
+  });
+
+  it('should not throw if not exists', done => {
+    const { _id } = Status.fake();
+    Status.getOneOrDefault({ _id }, (error, found) => {
+      expect(error).to.not.exist;
+      expect(found).to.exist;
+      expect(found._id).to.eql(status._id);
+      done(error, found);
+    });
+  });
+
+  after(done => clear(done));
+});
