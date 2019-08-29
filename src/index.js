eb6680b0c9c4a10601b07b4ccad25f29ef047426
lykmapipo <lallyelias87@gmail.com>
2019-08-29 17:54:03 +0300

    refactor: improve package script & main export style

--- src/index.js
+++ src/index.js
@@ -11,11 +11,8 @@
  * @license MIT
  * @example
  *
- * const { app } = require('@codetanzania/majifix-status');
- *
- * ...
- *
- * app.start();
+ * const { Status, start } = require('@codetanzania/majifix-status');
+ * start(error => { ... });
  *
  */
 
@@ -31,11 +28,10 @@ import statusRouter from './status.http.router';
  * @type {object}
  *
  * @author lally elias <lallyelias87@gmail.com>
- * @author Richard Aggrey <richardaggrey7@gmail.com>
+ * @author rijkerd <richardaggrey7@gmail.com>
  * @since 1.0.0
  * @version 0.1.0
  */
-
 export const info = pkg(
   `${__dirname}/package.json`,
   'name',
@@ -52,27 +48,23 @@ export const info = pkg(
 /**
  * @name Status
  * @description Status model
- * @type {mongoose.Model}
  *
  * @author lally elias <lallyelias87@gmail.com>
- * @author Richard Aggrey <richardaggrey7@gmail.com>
+ * @author rijkerd <richardaggrey7@gmail.com>
  * @since 0.1.0
  * @version 0.1.0
  */
-
 export { Status };
 
 /**
  * @name statusRouter
  * @description status http router
- * @type {express.Router}
  *
  * @author lally elias <lallyelias87@gmail.com>
- * @author Richard Aggrey <richardaggrey7@gmail.com>
+ * @author rijkerd <richardaggrey7@gmail.com>
  * @since 0.1.0
  * @version 0.1.0
  */
-
 export { statusRouter };
 
 /**
@@ -81,7 +73,7 @@ export { statusRouter };
  * @type {string}
  *
  * @author lally elias <lallyelias87@gmail.com>
- * @author Richard Aggrey <richardaggrey7@gmail.com>
+ * @author rijkerd <richardaggrey7@gmail.com>
  * @since 0.1.0
  * @version 0.1.0
  */
